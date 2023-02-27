28522f776156d7221ac06782ff7268b7137349fb
Leonid Mesentsev <leonid.mesentsev@accenture.com>
2023-02-27 16:02:37 +0100

    [FIX] adaptable colors

--- CPULoadMonitor/UI/StatusView.swift
+++ CPULoadMonitor/UI/StatusView.swift
@@ -18,13 +18,13 @@ final class StatusView: NSView {
         let borderPath = NSBezierPath(roundedRect: bounds.insetBy(dx: 0.5, dy: 0.5), xRadius: 4.0, yRadius: 4.0)
         borderPath.setClip()
 
-        NSColor.green.set()
+        NSColor(calibratedRed: 0.0, green: 0.81, blue: 0.35, alpha: 0.99).set()
         var rect = bounds.insetBy(dx: 1.0, dy: 1.0)
         rect.size.width = rect.width * value
         let fillPath = NSBezierPath(rect: rect)
         fillPath.fill()
 
-        NSColor.init(white: 1.0, alpha: 1.0).set()
+        NSColor.labelColor.set()
         borderPath.lineWidth = 1.5
         borderPath.stroke()
 
@@ -33,7 +33,7 @@ final class StatusView: NSView {
         let attrs: [NSAttributedString.Key: Any] = [
             .font: NSFont(name: "HelveticaNeue", size: 10)!,
             .paragraphStyle: paragraphStyle,
-            .foregroundColor: NSColor.white
+            .foregroundColor: NSColor.labelColor
         ]
         let string = "\(Int(value * 100))%"
         string.draw(with: bounds.insetBy(dx: 0, dy: 4.5), options: [], attributes: attrs, context: nil)
